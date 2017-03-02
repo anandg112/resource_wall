8922a33b9a2c5f2adc5347aa2a9f491ce4dc2bb7
macky <mackybeltran1@gmail.com>
2017-03-02 23:47:12 +0000

    seeds all added

--- db/seeds/zmovies.js
+++ db/seeds/zmovies.js
@@ -5,7 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('movies').insert({id: 1, youtubeid: 'Yqq91WC3XWk', title: 'test', description: 'test', likes: 1, id_user: 1, tag_id: 1})
+        knex('movies').insert({id: 1, youtubeid: 'Yqq91WC3XWk', title: 'test', description: 'test', likes: 1, id_user: 1, tag_id: 1}),
+        knex('movies').insert({id: 2, youtubeid: 'dQw4w9WgXcQ', title: 'test2', description: 'test2', likes: 1, id_user: 1, tag_id: 1})
       ]);
     });
 };
