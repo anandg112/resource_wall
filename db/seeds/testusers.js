f0fb919aed0beefb49e7de286370a6df2d560770
macky <mackybeltran1@gmail.com>
2017-03-02 21:32:35 +0000

    adding seed data

---
+++ db/seeds/testusers.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('users').insert({id: 1, email: 'test@test.com', password: 'test', first_name: 'test', last_name: 'test'}),
+        knex('users').insert({id: 2, email: 'example@example.com', password: 'example', first_name: 'example', last_name: 'example'}),
+        knex('users').insert({id: 3, email: 'user@user.com', password: 'user', first_name: 'user', last_name: 'user'})
+      ]);
+    });
+};
