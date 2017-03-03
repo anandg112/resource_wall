ec5cf064a0ef365597fbb97ab3dca4182ef96bc4
Anand <anandg112@gmail.com>
2017-03-03 20:42:00 +0000

    working on showing user videos

---
+++ routes/content.js
@@ -0,0 +1,29 @@
+// knex
+//   .select('*')
+//   .from('movies')
+//   .where('youtubeid', 'Yqq91WC3XWk')
+//   .then((results) => {
+//     console.log(results);
+//   })
+//   .catch((error)=>{
+//     console.log(error);
+//   });
+
+// "use strict";
+//
+// const express = require('express');
+// const router  = express.Router();
+//
+// module.exports = (knex) => {
+//
+//   router.get("/", (req, res) => {
+//     knex
+//       .select("*")
+//       .from("users")
+//       .then((results) => {
+//         res.json(results);
+//     });
+//   });
+//
+//   return router;
+// }
