f84a4e161f671594b8c3c7785202b5b877e9d31e
Lucia416 <lucia.awang416@gmail.com>
2017-03-05 01:31:15 +0000

    fe changes

---
+++ public/scripts/lucy.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".material-icons").click(function(){
+
+    $.ajax({
+      method:"POST",
+      url:"/users/:likes"
+    })
+    .done(function(){
+      $
+    })
+
+  })
+
+})
