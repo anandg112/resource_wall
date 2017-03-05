161cabdde36c3a76e4ab86226b0ca05690d2ff05
Lucia416 <lucia.awang416@gmail.com>
2017-03-05 23:31:41 +0000

    other changes for the like

--- public/scripts/lucy.js
+++ public/scripts/lucy.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
-  $(".material-icons").click(function(){
+  $("#like").click(function(){
+    let id = this.attr('movieId')
+    let likes = this.attr('likes')
 
     $.ajax({
       method:"POST",
-      url:"/users/:likes"
+      url:"/users/:likes",
+      data: {
+        id: id ,
+        likes: likes
+      }
     })
-    .done(function(){
-    $(".material-icons").hide();
-    $(".icons-color").show();
+    .done(function(response){
+    $("#like").css("display");
+    $("#like-change").css("display", "none");
+    // loadlikes();
 
     })
 
   })
 
-})
+});
