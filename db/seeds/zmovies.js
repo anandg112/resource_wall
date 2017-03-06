5ec278c224804f5dab5f11a5d201173d9883ffd4
macky <mackybeltran1@gmail.com>
2017-03-06 12:05:18 +0000

    adding some more seeds

--- db/seeds/zmovies.js
+++ db/seeds/zmovies.js
@@ -14,6 +14,10 @@ exports.seed = function(knex, Promise) {
         knex('movies').insert({id: 7, youtubeid: 'EZfXa8vO8N8', title: 'Jamie Oliver', description: 'Jamie Oliver cooks', likes: 13, id_user: 5, tag_id: 4}),
         knex('movies').insert({id: 8, youtubeid: 'YxiqHlINZGI', title: 'Learn The Fundamentals of CARPENTRY from ANTHONY GILARDI', description: 'Carpentry for Beginners!', likes: 1, id_user: 4, tag_id: 7}),
         knex('movies').insert({id: 9, youtubeid: 'emm3TCDWOUU', title: 'LCar Repair Marathon', description: 'Car Repair!!', likes: 6, id_user: 5, tag_id: 5}),
+        knex('movies').insert({id: 10, youtubeid: 'CHaTWk6uqd8', title: 'How to Adjust Gears & Derailleurs | Bicycle Repair', description: 'Summers almost here, lets learn how to repair a bike!', likes: 10, id_user: 6, tag_id: 8}),
+        knex('movies').insert({id: 11, youtubeid: 'J7QFNimcLQM', title: 'More Bike Stuff!', description: 'fixing gears', likes: 2, id_user: 6, tag_id: 8}),
+        knex('movies').insert({id: 12, youtubeid: 'zZ8aqP2NWk8', title: 'Arnold: Pumping Iron', description: 'look at those pecs!', likes: 3, id_user: 7, tag_id: 9}),
+        knex('movies').insert({id: 13, youtubeid: 'CorfPVWZGfI', title: 'Damn Son!', description: 'So buff, these guys!', likes: 14, id_user: 7, tag_id: 9}),
       ]);
     });
 };
